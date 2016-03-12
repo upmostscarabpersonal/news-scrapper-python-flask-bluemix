1a9f5094e02cec7300f4760fee65ba0371dec7b0
UpmostScarab <fuzzykiller2@gmail.com>
2016-03-12 17:16:02 +0300

    Added files via upload

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect,request
+import bs4
+import requests
+import os
+app = Flask(__name__)
+
+
+def scrape_site(URL, keyword):
+    response = requests.get(URL)
+    soup = bs4.BeautifulSoup(response.text)
+    results = [ [a.attrs.get('href'), a.contents[0]] for a in soup.select('a[href^=' + URL + ']') if keyword in a.contents[0] ]
+    results = results + [ [URL + a.attrs.get('href'), a.contents[0]] for a in soup.select('a[href^=/]')  if keyword in a.contents[0] ]
+    b_set = set(tuple(x) for x in results)
+    b = [ list(x) for x in b_set ]
+    b.sort(key = lambda x: results.index(x) )
+    results = b
+    return results
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/scrape', methods = ['POST'])
+def scrape_and_show():
+    URL = request.form['url']
+    keyword = request.form['keyword']
+    result = scrape_site(URL, keyword)
+    return render_template('results.html', keyword=keyword, URL=URL, result=result)
+
+@app.route('/back')
+def home():
+    return redirect("/")
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
